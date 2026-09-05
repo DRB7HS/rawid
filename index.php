0c1ee8b2d9d224269f134866f4963f2d330959dc
DRB7H <raneemm.marwan@gmail.com>
2026-09-05 20:38:06 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,14 @@
 <?php
+
 header("Content-Type: text/plain; charset=utf-8");
-$scriptContent = file_get_contents('https://pastefy.app/Ld8m1JVK/raw');
+
+$url = "https://pastefy.app/Ld8m1JVK/raw";
+$scriptContent = file_get_contents($url);
+
+if ($scriptContent === false) {
+    http_response_code(500);
+    exit("Failed to fetch content.");
+}
+
 echo $scriptContent;
 ?>
